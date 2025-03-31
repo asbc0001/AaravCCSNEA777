ca658e3109783817e2af18b37eac4ba6d1a3381e
asbc0001 <asbchheda@gmail.com>
2025-03-31 18:17:24 +0100

    Created initial flask app

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+
+# Generic route for testing that the Flask application works, will be removed after
 @app.route('/')
 def index():
     return "Hello"
+
