0aae05499faeeb9d11f3f01a15c58965da9c5111
asbc0001 <asbchheda@gmail.com>
2025-03-31 19:10:31 +0100

    Set secret key

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
 
 # Create a Flask application instance
 app = Flask(__name__)
 
+# Retrieve secret key from environment variable and set it
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # Generic route for testing that the Flask application works, will be removed after
 @app.route('/')
