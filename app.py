a9674118c2d90d67d4757b1200c8343e60789d5e
asbc0001 <asbchheda@gmail.com>
2025-03-31 21:41:22 +0100

    Imported SQLAlchemy, cofigured URI

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 # Retrieve secret key from environment variable and set it
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+#
+basedirectory = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedirectory, 'tracker.db')
 
-# Generic route for testing that the Flask application works, will be removed after
+# Generic route for testing that the Flask application works
 @app.route('/')
 def index():
     return "Hello"
