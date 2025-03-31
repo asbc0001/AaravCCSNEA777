d8bf77614eb1848d565eff9f269cdcd621f7e74a
asbc0001 <asbchheda@gmail.com>
2025-03-31 21:50:13 +0100

    Imported SQLAlchemy

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
+
+# Get the path of the directory where the app.py script is located
+basedirectory = os.path.abspath(os.path.dirname(__file__))
 
 # Create a Flask application instance
 app = Flask(__name__)
 
 # Retrieve secret key from environment variable and set it
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-#
-basedirectory = os.path.abspath(os.path.dirname(__file__))
+# Configure the SQLite database URI by joining the base directory with 'tracker.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedirectory, 'tracker.db')
+
+# Initialize SQLAlchemy for the Flask app
+db = SQLAlchemy(app)
 
 # Generic route for testing that the Flask application works
 @app.route('/')
