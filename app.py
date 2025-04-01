891bda8289ef6a8c7b87ece44425bb62e98ee572
asbc0001 <asbchheda@gmail.com>
2025-04-01 20:38:37 +0100

    Created user model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 import os
 
 # Get the path of the directory where the app.py script is located
@@ -15,6 +16,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedirectory,
 
 # Initialize SQLAlchemy for the Flask app
 db = SQLAlchemy(app)
+
+
+#
+class User(db.Model):
+    id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
+    email: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(120), unique=True)
+    password_hash: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(255))
+    sets_unit: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(2), default="kg")
+    bodyweight_unit: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(2), default="kg")
+    receive_reports: sa.orm.Mapped[bool] = sa.orm.mapped_column(sa.Boolean, default = True)
+    
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+
 
 # Generic route for testing that the Flask application works
 @app.route('/')
