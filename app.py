8018dee976a714d1db3d7771ee004b45235f1eef
asbc0001 <asbchheda@gmail.com>
2025-04-03 10:29:16 +0100

    Set model

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedirectory,
 db = SQLAlchemy(app)
 
 
-# Model to define the structure of the database User table
+# Model to define the structure of the database user table
 class User(db.Model):
     user_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     email: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(120), unique=True)
@@ -38,11 +38,14 @@ class User(db.Model):
     # Define relationship to Goal model
     goals: sa.orm.Mapped[list["Goal"]] = sa.orm.relationship(back_populates="user")
     
+    # Define relationship to Set model
+    sets: sa.orm.Mapped[list["Set"]] = sa.orm.relationship(back_populates="user")
+    
     # Method for providing string representation (email) of the User instance for debugging/logging purposes
     def __repr__(self):
         return f"<User {self.email}>"
 
-# Model to define the structure of the database Bodyweight table
+# Model to define the structure of the database bodyweight table
 class Bodyweight(db.Model):
     weight_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     weight: sa.orm.Mapped[float] = sa.orm.mapped_column(sa.Float)
@@ -55,9 +58,9 @@ class Bodyweight(db.Model):
     
     # Displays data about a Bodyweight entry for debugging / logging purposes
     def __repr__(self):
-        return f"<Bodyweight {self.weight} {self.user.bodyweight_unit} on {self.date} for User {self.user_id}>"
+        return f"<Bodyweight {self.weight} on {self.date} for User {self.user_id}>"
  
-# Model to define structure of the database Exercise table
+# Model to define structure of the database exercise table
 class Exercise(db.Model):
     exercise_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     name: sa.orm.Mapped[str] = sa.orm.mapped_column(sa.String(50))
@@ -70,11 +73,14 @@ class Exercise(db.Model):
     # Define relationship to Goal model
     goals: sa.orm.Mapped[list["Goal"]] = sa.orm.relationship(back_populates="exercise", cascade="all, delete")
     
+    #Define relationship to Set model
+    sets: sa.orm.Mapped[list["Set"]] = sa.orm.relationship(back_populates="exercise", cascade="all, delete")
+    
     # Display data about an Exercise entry for debugging / logging purposes
     def __repr__(self):
         return f"<Exercise {self.name} for User {self.user_id}>"
 
-# Model to define structure of the database Goal table    
+# Model to define structure of the database goal table    
 class Goal(db.Model):
     goal_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     target: sa.orm.Mapped[float] = sa.orm.mapped_column(sa.Float)
@@ -91,6 +97,26 @@ class Goal(db.Model):
     # Display data about a Goal entry for debugging / logging purposes
     def __repr__(self):
         return f"<Goal {self.target} for Exercise {self.exercise_id} for User {self.user_id}>"
+
+# Model to define structure of the database set table
+class Set(db.Model):
+    set_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, primary_key=True, autoincrement=True)
+    weight: sa.orm.Mapped[float] = sa.orm.mapped_column(sa.Float)
+    reps: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer)
+    estimated_1RM: sa.orm.Mapped[float] = sa.orm.mapped_column(sa.Float)
+    date: sa.orm.Mapped[datetime.date] = sa.orm.mapped_column(sa.Date)
+    user_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, sa.ForeignKey("user.user_id"))
+    exercise_id: sa.orm.Mapped[int] = sa.orm.mapped_column(sa.Integer, sa.ForeignKey("exercise.exercise_id"))
+    
+    # Define relationship to User model
+    user: sa.orm.Mapped[User] = sa.orm.relationship(back_populates="sets")
+    
+    # Define relationship to Exercise model
+    exercise: sa.orm.Mapped[Exercise] = sa.orm.relationship(back_populates="sets")
+    
+    # Display data about a Set entry for debugging / logging purposes
+    def __repr__(self):
+        return f"<Weight {self.weight} for reps {self.reps} on date {self.date} for exercise {self.exercise_id} for User {self.user_id}>"
 
 # Generic route for testing that the Flask application works
 @app.route('/')
