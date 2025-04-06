376e7d5a9c802c41c4f25362e43dc190755a7973
asbc0001 <asbchheda@gmail.com>
2025-04-06 12:38:15 +0100

    Register route complete

---
+++ defaultexercises.py
@@ -0,0 +1,49 @@
+
+# define list of dictionaries of default exercises to add to exercise table whenever a new user is created
+default_exercises =  [
+    {"name": "Barbell Row", "category": "Back"},
+    {"name": "Barbell Shrug", "category": "Back"},
+    {"name": "Chin up", "category": "Back"},
+    {"name": "Deadlift", "category": "Back"},
+    {"name": "Dumbbell Row", "category": "Back"},
+    {"name": "Lat Pulldown", "category": "Back"},
+    {"name": "Pull Up", "category": "Back"},
+    {"name": "Seated Cable Row", "category": "Back"},
+    
+    {"name": "Barbell Curl", "category": "Biceps"},
+    {"name": "Barbell Preacher Curl", "category": "Biceps"},
+    {"name": "Cable Curl", "category": "Biceps"},
+    {"name": "Dumbbell Curl", "category": "Biceps"},
+    {"name": "Dumbbell Hammer Curl", "category": "Biceps"},
+    {"name": "Dumbbell Preacher Curl", "category": "Biceps"},
+    
+    {"name": "Flat Barbell Bench Press", "category": "Chest"},
+    {"name": "Flat Dumbbell Bench Press", "category": "Chest"},
+    {"name": "Incline Barbell Bench Press", "category": "Chest"},
+    {"name": "Incline Dumbbell Bench Press", "category": "Chest"},
+    {"name": "Push-Up", "category": "Chest"},
+    
+    {"name": "Dumbbell Reverse Grip Curl", "category": "Forearms"},
+    {"name": "Wrist Curl", "category": "Forearms"},
+    {"name": "Reverse Wrist Curl", "category": "Forearms"},
+    
+    {"name": "Barbell Front Squat", "category": "Legs"},
+    {"name": "Barbell Squat", "category": "Legs"},
+    {"name": "Leg Extension", "category": "Legs"},
+    {"name": "Leg Press", "category": "Legs"},
+    {"name": "Romanian Deadlift", "category": "Legs"},
+    {"name": "Seated Calf Raise", "category": "Legs"},
+    {"name": "Split Squat", "category": "Legs"},
+    {"name": "Standing Calf Raise", "category": "Legs"},
+    
+    {"name": "Cable Face Pull", "category": "Shoulders"},
+    {"name": "Front Dumbbell Raise", "category": "Shoulders"},
+    {"name": "Lateral Dumbbell Raise", "category": "Shoulders"},
+    {"name": "Overhead Press", "category": "Shoulders"},
+    
+    {"name": "Close Grip Barbell Bench Press", "category": "Triceps"},
+    {"name": "Diamond Push-Up", "category": "Triceps"},
+    {"name": "Dip", "category": "Triceps"},
+    {"name": "Rope Pushdown", "category": "Triceps"},
+    {"name": "Skull Crusher", "category": "Triceps"}
+]
